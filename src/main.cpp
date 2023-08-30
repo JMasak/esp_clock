805be8ebe7e8735114197a929e31fae94b5568d3
Jan Masak <Jan.Masak@sbdinc.com>
2023-08-30 15:50:54 +0200

    some more time

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 Arduino_DataBus *bus = new Arduino_ESP32SPI(2 /* DC */, 10 /* CS */, 6 /* SCK */, 7 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *gfx = new Arduino_GC9A01(bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */, true /* IPS */);
 
+unsigned long last_millis = 0;
+time_t muh = 0;
+
 static const tmElements_t Ende = { 
   0, 
   0, 
@@ -17,18 +20,31 @@ static const tmElements_t Ende = {
 
 static const tmElements_t Now = { 
   0, 
-  5, 
-  10, 
+  44, 
+  15, 
   4,   // day of week, sunday is day 1
   30,
   8,
   53,  //2023 - 1970
 };
 
+union Color
+{
+    struct Colors
+    {
+        uint16_t    red:5;
+        uint16_t    green:6;
+        uint16_t    blue:5;
+    };
+    uint16_t value;
+};
+
+
 void setup() {
   // put your setup code here, to run once:
-  setTime(makeTime(Now));
-  time_t noch = elapsedDays(makeTime(Ende) - makeTime(Now));
+  muh = makeTime(Now);
+  setTime(muh);
+  last_millis = millis();
 
   //gfx->fillScreen(BLACK);
   pinMode(GFX_BL, OUTPUT);
@@ -36,16 +52,16 @@ void setup() {
   gfx->begin();
   gfx->fillScreen(BLACK);
   gfx->fillRoundRect(80,80,80,80,15,RED);
-  delay(5000);
-  gfx->fillScreen(BLUE);
-  gfx->setTextSize(3);
-  gfx->printf("\n\n\n     noch\n     %d\n     Tage\n", noch);  
+  delay(2000);
+  gfx->setTextSize(3);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  gfx->invertDisplay(false);
-  delay(5000);
-  gfx->invertDisplay(true);
-  delay(5000);
+  setTime(muh + (millis()-last_millis) / 1000);
+  time_t noch = elapsedDays(makeTime(Ende) - now());
+  gfx->fillScreen(BLUE);
+  gfx->setCursor(50, 60);
+  gfx->printf("%02d:%02d:%02d\n\n     noch\n     %d\n     Tage\n", hour(), minute(), second(), noch);
+  delay(500);
 }
