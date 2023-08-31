efd18178e7fdebbbc2926f791001bbdfe6b3a1f5
Jan Masak <Jan.Masak@sbdinc.com>
2023-08-31 07:34:25 +0200

    use backbuffer to reduce flicker... uses only 3bits for now

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,38 @@
 #include <TimeLib.h>
 #define GFX_BL DF_GFX_BL
 Arduino_DataBus *bus = new Arduino_ESP32SPI(2 /* DC */, 10 /* CS */, 6 /* SCK */, 7 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
-Arduino_GFX *gfx = new Arduino_GC9A01(bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */, true /* IPS */);
+Arduino_GFX *output_display = new Arduino_GC9A01(bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */, true /* IPS */);
+Arduino_GFX *gfx = new Arduino_Canvas_3bit(240 /* width */, 240 /* height */, output_display, 0 /* output_x */, 0 /* output_y */);
 
 unsigned long last_millis = 0;
 time_t muh = 0;
+
+// static const tmElements_t Ende = { 
+//   0, 
+//   0, 
+//   12, 
+//   6,   // day of week, sunday is day 1
+//   31,
+//   7,
+//   56, //2026 - 1970
+// };
 
 static const tmElements_t Ende = { 
   0, 
   0, 
   12, 
   6,   // day of week, sunday is day 1
-  31,
-  7,
-  56, //2026 - 1970
+  28,
+  2,
+  78, //2026 - 1970
 };
 
 static const tmElements_t Now = { 
   0, 
-  44, 
-  15, 
-  4,   // day of week, sunday is day 1
-  30,
+  30, 
+  7, 
+  5,   // day of week, sunday is day 1
+  31,
   8,
   53,  //2023 - 1970
 };
@@ -63,5 +74,6 @@ void loop() {
   gfx->fillScreen(BLUE);
   gfx->setCursor(50, 60);
   gfx->printf("%02d:%02d:%02d\n\n     noch\n     %d\n     Tage\n", hour(), minute(), second(), noch);
+  gfx->flush();
   delay(500);
 }
