0c6edd5f5dae7ac5beb99baa572cad93cf33c81a
Jan Masak <Jan.Masak@sbdinc.com>
2023-08-31 13:25:25 +0200

    add analog watch

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,41 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 #include <TimeLib.h>
-#define GFX_BL DF_GFX_BL
+
+#define LCD_WIDTH   240
+#define LCD_HEIGHT  240
+#define GFX_BL      DF_GFX_BL
 Arduino_DataBus *bus = new Arduino_ESP32SPI(2 /* DC */, 10 /* CS */, 6 /* SCK */, 7 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
 Arduino_GFX *output_display = new Arduino_GC9A01(bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */, true /* IPS */);
-Arduino_GFX *gfx = new Arduino_Canvas_3bit(240 /* width */, 240 /* height */, output_display, 0 /* output_x */, 0 /* output_y */);
+Arduino_GFX *gfx = new Arduino_Canvas_3bit(LCD_WIDTH, LCD_HEIGHT, output_display, 0 /* output_x */, 0 /* output_y */);
 
 unsigned long last_millis = 0;
-time_t muh = 0;
-
-// static const tmElements_t Ende = { 
-//   0, 
-//   0, 
-//   12, 
-//   6,   // day of week, sunday is day 1
-//   31,
-//   7,
-//   56, //2026 - 1970
-// };
+time_t last_time = 0;
 
 static const tmElements_t Ende = { 
   0, 
   0, 
   12, 
   6,   // day of week, sunday is day 1
-  28,
-  2,
-  78, //2026 - 1970
+  31,
+  7,
+  56, //2026 - 1970
 };
+
+// static const tmElements_t Ende = { 
+//   0, 
+//   0, 
+//   12, 
+//   6,   // day of week, sunday is day 1
+//   28,
+//   2,
+//   78, //2026 - 1970
+// };
 
 static const tmElements_t Now = { 
   0, 
-  30, 
-  7, 
+  26, 
+  10, 
   5,   // day of week, sunday is day 1
   31,
   8,
@@ -53,27 +56,31 @@ union Color
 
 void setup() {
   // put your setup code here, to run once:
-  muh = makeTime(Now);
-  setTime(muh);
+  last_time = makeTime(Now);
+  setTime(last_time);
   last_millis = millis();
 
-  //gfx->fillScreen(BLACK);
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, HIGH);
   gfx->begin();
   gfx->fillScreen(BLACK);
-  gfx->fillRoundRect(80,80,80,80,15,RED);
-  delay(2000);
   gfx->setTextSize(3);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  setTime(muh + (millis()-last_millis) / 1000);
+  setTime(last_time + (millis()-last_millis) / 1000);
   time_t noch = elapsedDays(makeTime(Ende) - now());
-  gfx->fillScreen(BLUE);
-  gfx->setCursor(50, 60);
-  gfx->printf("%02d:%02d:%02d\n\n     noch\n     %d\n     Tage\n", hour(), minute(), second(), noch);
+  gfx->fillScreen(BLACK);
+  double x = sin(PI / 30.0 * (minute() + second() / 60.0)) * LCD_WIDTH / 2.0 + LCD_WIDTH / 2.0;
+  double y = (cos(PI / 30.0 * (minute() + second() / 60.0)) * LCD_HEIGHT / 2.0) * -1.0 + LCD_HEIGHT / 2.0;
+  gfx->drawLine(LCD_WIDTH/2, LCD_HEIGHT/2, x, y, YELLOW);
+  x = sin(PI / 6.0 * (hour() + minute() / 60.0)) * LCD_WIDTH / 3.0 + LCD_WIDTH / 2.0;
+  y = (cos(PI / 6.0 * (hour()  + minute() / 60.0)) * LCD_HEIGHT / 3.0) * -1.0 + LCD_HEIGHT / 2.0;
+  gfx->drawLine(LCD_WIDTH/2, LCD_HEIGHT/2, x, y, YELLOW);
+  gfx->fillRoundRect(75, 99, 90, 42, 5, BLUE);
+  gfx->setCursor(86, 109);
+  gfx->printf("%d", noch);
   gfx->flush();
-  delay(500);
+  delay(10000);
 }
